e1a4027af551e8e09a7957a7bb3fcba48b5c29f4
francomac <franco.mcarballo@outlook.com>
2024-04-15 16:06:58 -0600

    beforeEach to reduce redundancy

--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -2,15 +2,21 @@ import { mount } from "@vue/test-utils";
 import AppTodo from "@/components/TodoApp/TodoApp.vue";
 
 describe("TodoApp", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(AppTodo);
+  });
+
   it("should render AppTodo text", () => {
-    const wrapper = mount(AppTodo);
+    // const wrapper = mount(AppTodo);
     const todo = wrapper.get("[data-test='todo']");
 
     expect(todo.text()).toBe("testing tut");
   });
 
   it("should add a new todo", async () => {
-    const wrapper = mount(AppTodo);
+    // const wrapper = mount(AppTodo);
 
     expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1);
 
@@ -23,7 +29,7 @@ describe("TodoApp", () => {
   });
 
   it("should be able to complete todo", async () => {
-    const wrapper = mount(AppTodo);
+    // const wrapper = mount(AppTodo);
 
     await wrapper.get('[data-test="todo-checkbox"]').setChecked(true);
 
